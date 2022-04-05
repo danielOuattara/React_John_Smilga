cf7e5e9816cb6c687fc40f48cc760a389b26ff42
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-05 22:11:51 +0200

    Url Parameter: productId setup and dynamic use

--- 22_React_Router_6/src/my_solution/pages/Products.js
+++ 22_React_Router_6/src/my_solution/pages/Products.js
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom";
+import products from "./../data";
+
 const Products = () => {
   return (
-    <>
-      <section className='section'>
-        <h2>products</h2>
-      </section>
-    </>
+    <section className="section">
+      <h2>products</h2>
+      {products.map((item) => {
+        return (
+          <article key={item.id}>
+            <h5>{item.name}</h5>
+            <Link to={`/products/${item.id}`}>more info</Link>
+          </article>
+        );
+      })}
+    </section>
   );
 };
 
--- 22_React_Router_6/src/my_solution/pages/SingleProduct.js
+++ 22_React_Router_6/src/my_solution/pages/SingleProduct.js
@@ -1,7 +1,31 @@
+// import { Link, useParams } from "react-router-dom";
+
+// const SingleProduct = () => {
+//   const { productId } = useParams();
+//   return (
+//     <section className="section product">
+//       <h2>single product {productId}</h2>
+//       <Link to="/products">back To Products</Link>
+//     </section>
+//   );
+// };
+
+// export default SingleProduct;
+
+//------------------------------------------------------------------------
+
+import { Link, useParams } from "react-router-dom";
+import products from "./../data";
+
 const SingleProduct = () => {
+  const { productId } = useParams();
+  const product = products.find((item) => item.id === productId);
+  const { image, name } = product;
   return (
-    <section className='section product'>
-      <h2>single product</h2>
+    <section className="section product">
+      <img src={image} alt={name} />
+      <h5>{name}</h5>
+      <Link to="/products">back To Products</Link>
     </section>
   );
 };
