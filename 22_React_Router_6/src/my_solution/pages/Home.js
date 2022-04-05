693f15d714e9e571535b54789ee7220dd967c397
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-05 17:27:37 +0200

    Navbar & SharedLayout

--- 22_React_Router_6/src/my_solution/pages/Home.js
+++ 22_React_Router_6/src/my_solution/pages/Home.js
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
+  <>
     <section className="section">
       <h2>Home Page</h2>
-      <Link to="/about" className="btn">About Page</Link>
+
+
     </section>
+  </>
   );
 };
 export default Home;
