412d3309c783718cd7666058a02bb79cc1f4efe8
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-05 21:12:34 +0200

    NavIink and class active coloration link

--- 22_React_Router_6/src/my_solution/pages/SharedLayout.js
+++ 22_React_Router_6/src/my_solution/pages/SharedLayout.js
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+// import Navbar from "../components/Navbar";
+import StyleNavbar from "./../components/StyleNavbar";
 const SharedLayout = () => {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
+      <StyleNavbar />
       <Outlet />
     </>
   );
