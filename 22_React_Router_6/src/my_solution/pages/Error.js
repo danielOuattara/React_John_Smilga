65bf70141988c5640c74a6faac881c24c73da2c5
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-05 16:09:38 +0200

    404 not Found: using * as any other route

--- 22_React_Router_6/src/my_solution/pages/Error.js
+++ 22_React_Router_6/src/my_solution/pages/Error.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
-    <section className='section'>
-      <h2>Error</h2>
+    <section className="section">
+      <h2>Error - 404</h2>
+      <p>Page Not Found</p>
+      <Link to="/" className="btn">Back Home</Link>
     </section>
   );
 };
