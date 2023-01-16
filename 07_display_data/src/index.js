35d74d81e5dc3592d370225502438828054d85e6
danielOuattara <daniel.ouattara@gmx.com>
2023-01-16 15:01:26 +0100

    update logic + formatting

--- 07_display_data/src/index.js
+++ 07_display_data/src/index.js
@@ -10,8 +10,7 @@
 //     author: "Amelia Hepworth",
 //   },
 //   {
-//     imgLink:
-//       "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
+//     imgLink: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
 //     title: "Our Class is a family",
 //     author: "Shannon Olsen",
 //   },
@@ -33,44 +32,34 @@
 //   );
 // }
 
-//-------------------
+// // -------------------
 // function BoookList2() {
 //   return (
 //     <section className="booklist">
 //       {books.map((book) => {
 //         const { imgLink, title, author } = book;
 //         return (
-//           <Book
-//             imgLink={imgLink}
-//             title={title}
-//             author={author}
-//             key={title}
-//           />
+//           <Book imgLink={imgLink} title={title} author={author} key={title} />
 //         );
 //       })}
 //     </section>
 //   );
 // }
 
-//---------------------
+// // ---------------------
 // function BoookList3() {
 //   return (
 //     <section className="booklist">
 //       {books.map(({ imgLink, title, author }) => {
 //         return (
-//           <Book
-//             imgLink={imgLink}
-//             title={title}
-//             author={author}
-//             key={title}
-//           />
+//           <Book imgLink={imgLink} title={title} author={author} key={title} />
 //         );
 //       })}
 //     </section>
 //   );
 // }
 
-//---------------------
+// // ---------------------
 
 // const Book = (props) => {
 //   const { imgLink, title, author } = props;
@@ -91,7 +80,7 @@
 //     <hr />
 //     <BoookList3 />
 //   </>,
-//   document.getElementById("root")
+//   document.getElementById("root"),
 // );
 
 // ------------------------------------------------------------------------
@@ -107,8 +96,7 @@
 //     author: "Amelia Hepworth",
 //   },
 //   {
-//     imgLink:
-//       "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
+//     imgLink: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
 //     title: "Our Class is a family",
 //     author: "Shannon Olsen",
 //   },
@@ -153,15 +141,13 @@
 //   },
 //   {
 //     id: 2,
-//     imgLink:
-//       "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
+//     imgLink: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
 //     title: "Our Class is a family",
 //     author: "Shannon Olsen",
 //   },
 //   {
 //     id: 3,
-//     imgLink:
-//       "https://m.media-amazon.com/images/I/61VudanhEXL._AC_UL320_.jpg",
+//     imgLink: "https://m.media-amazon.com/images/I/61VudanhEXL._AC_UL320_.jpg",
 //     title: "The Vanishing Half",
 //     author: "Brit Bennett",
 //   },
@@ -183,7 +169,7 @@
 //   return (
 //     <section className="booklist">
 //       {books.map((book) => {
-//         return <Book book={book} key={book.id} />;
+//         return <Book2 book={book} key={book.id} />;
 //       })}
 //     </section>
 //   );
@@ -217,85 +203,85 @@
 //   <>
 //     <BoookList /> <BoookList2 />{" "}
 //   </>,
-//   document.getElementById("root")
+//   document.getElementById("root"),
 // );
 
 // USING SPREAD OPERATOR ON OBJECT
 
 // ------------------------------------------------------------------------
 
-// import ReactDOM from "react-dom";
-// import "./index.css";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-// const books = [
-//   {
-//     id: 1,
-//     imgLink:
-//       "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-//     title: "I Love You to the Moon and Back",
-//     author: "Amelia Hepworth",
-//   },
-//   {
-//     id: 2,
-//     imgLink:
-//       "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
-//     title: "Our Class is a family",
-//     author: "Shannon Olsen",
-//   },
-//   {
-//     id: 3,
-//     imgLink:
-//       "https://m.media-amazon.com/images/I/61VudanhEXL._AC_UL320_.jpg",
-//     title: "The Vanishing Half",
-//     author: "Brit Bennett",
-//   },
-// ];
+const books = [
+  {
+    id: 1,
+    imgLink:
+      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
+  {
+    id: 2,
+    imgLink: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
+    title: "Our Class is a family",
+    author: "Shannon Olsen",
+  },
+  {
+    id: 3,
+    imgLink: "https://m.media-amazon.com/images/I/61VudanhEXL._AC_UL320_.jpg",
+    title: "The Vanishing Half",
+    author: "Brit Bennett",
+  },
+];
 
-// - 1 -
-// function BoookList() {
-//   return(
-//     <section className="booklist">
-//         {books.map(book => (
-//             <Book {...book} key={book.title}/>))}
-//     </section>
-//   );
-// }
+// -1 -
+function BoookList1() {
+  return (
+    <section className="booklist">
+      {books.map((book) => (
+        <Book1 {...book} key={book.title} />
+      ))}
+    </section>
+  );
+}
+
+const Book1 = ({ imgLink, title, author }) => {
+  // changed
+  return (
+    <article className="book">
+      <img src={imgLink} alt="book description" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
 
 // - 2 -
 // -------------------
-// function BoookList() {
-//   return (
-//     <section className="booklist">
-//       {books.map((book) => {
-//         return <Book {...book} key={book.id} />;
-//       })}
-//     </section>
-//   );
-// }
+function BoookList2() {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book2 {...book} key={book.title} />;
+      })}
+    </section>
+  );
+}
 
-// const Book = ({imgLink, title, author}) => { // changed
-//   return (
-//     <article className="book">
-//       <img
-//         src= {imgLink}
-//         alt="book description"
-//       />
-//       <h1>{title}</h1>
-//       <h4>{author}</h4>
-//     </article>
-//   );
-// }
+const Book2 = (props) => {
+  // changed
+  const { imgLink, title, author } = props;
+  return (
+    <article className="book">
+      <img src={imgLink} alt="book description" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
 
-// const Book = (props) => {
-//   // changed
-//   const { imgLink, title, author } = props;
-//   return (
-//     <article className="book">
-//       <img src={imgLink} alt="book description" />
-//       <h1>{title}</h1>
-//       <h4>{author}</h4>
-//     </article>
-//   );
-// };
-
-// ReactDOM.render(<BoookList />, document.getElementById("root"));
+ReactDOM.render(
+  [<BoookList1 />, <BoookList2 />],
+  document.getElementById("root"),
+);
