398c809a1584a2cf93a1cdd4dde7deb17638b80a
danielOuattara <daniel.ouattara@gmx.com>
2023-01-16 15:01:00 +0100

    update logic for props

--- 06_props/src/index.js
+++ 06_props/src/index.js
@@ -1,58 +1,64 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
 
 // function BoookList() {
 //   return (
 //     <section className="booklist">
 //       <Book title={title} />
-//       <Book author={author}/>
-
+//       <Book author={author} />
 //     </section>
 //   );
 // }
 
 // const title = "I Love You to the Moon and Back";
 // const author = "Amelia Hepworth";
-// const imgLink = "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+// const imgLink =
+//   "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg";
 
 // const Book = (props) => {
 //   return (
 //     <article className="book">
-//       <img
-//         src= {imgLink}
-//         alt="book description"
-//       />
+//       <img src={imgLink} alt="book description" />
 //       <h1>{props.title}</h1>
 //       <h4>{props.author}</h4>
 //     </article>
 //   );
-// }
+// };
 
-// ReactDOM.render(<BoookList/>, document.getElementById('root'));
+// ReactDOM.render(<BoookList />, document.getElementById("root"));
 
 // ------------------------------------------------------------------------------
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
 
-// const firstBook= {
-//   imgLink: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-//   title:"I Love You to the Moon and Back",
+// const firstBook = {
+//   imgLink:
+//     "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+//   title: "I Love You to the Moon and Back",
 //   author: "Amelia Hepworth",
-// }
-// const secondBook= {
+// };
+// const secondBook = {
 //   imgLink: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
-//   title:"Our Class is a family",
+//   title: "Our Class is a family",
 //   author: "Shannon Olsen",
-// }
+// };
 
 // function BoookList() {
-//   return(
+//   return (
 //     <section className="booklist">
-//       <Book title={firstBook.title} imgLink={firstBook.imgLink} author={firstBook.author} />
-//       <Book title={secondBook.title} imgLink={secondBook.imgLink} author={secondBook.author} />
+//       <Book
+//         title={firstBook.title}
+//         imgLink={firstBook.imgLink}
+//         author={firstBook.author}
+//       />
+//       <Book
+//         title={secondBook.title}
+//         imgLink={secondBook.imgLink}
+//         author={secondBook.author}
+//       />
 //     </section>
 //   );
 // }
@@ -60,41 +66,47 @@
 // const Book = (props) => {
 //   return (
 //     <article className="book">
-//       <img
-//         src= {props.imgLink}
-//         alt="book description"
-//       />
+//       <img src={props.imgLink} alt="book description" />
 //       <h1>{props.title}</h1>
 //       <h4>{props.author}</h4>
 //     </article>
 //   );
-// }
+// };
 
-// ReactDOM.render(<BoookList/>, document.getElementById('root'))
+// ReactDOM.render(<BoookList />, document.getElementById("root"));
 
 // ------------------------------------------------------------------------------
 // destructure props
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
 
-// const firstBook= {
-//   imgLink: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-//   title:"I Love You to the Moon and Back",
+// const firstBook = {
+//   imgLink:
+//     "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+//   title: "I Love You to the Moon and Back",
 //   author: "Amelia Hepworth",
-// }
-// const secondBook= {
+// };
+// const secondBook = {
 //   imgLink: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
-//   title:"Our Class is a family",
+//   title: "Our Class is a family",
 //   author: "Shannon Olsen",
-// }
+// };
 
 // function BoookList() {
-//   return(
+//   return (
 //     <section className="booklist">
-//       <Book title= {firstBook.title} imgLink={firstBook.imgLink} author={firstBook.author} />
-//       <Book title= {secondBook.title} imgLink={secondBook.imgLink} author={secondBook.author} />
+//       <Book
+//         title={firstBook.title}
+//         imgLink={firstBook.imgLink}
+//         author={firstBook.author}
+//       />
+//       <Book
+//         title={secondBook.title}
+//         imgLink={secondBook.imgLink}
+//         author={secondBook.author}
+//       />
 //     </section>
 //   );
 // }
@@ -103,17 +115,14 @@
 //   const { imgLink, title, author } = props;
 //   return (
 //     <article className="book">
-//       <img
-//         src={imgLink}
-//         alt="book description"
-//       />
+//       <img src={imgLink} alt="book description" />
 //       <h1>{title}</h1>
 //       <h4>{author}</h4>
 //     </article>
 //   );
-// }
+// };
 
-// ReactDOM.render(<BoookList/>, document.getElementById('root'));
+// ReactDOM.render(<BoookList />, document.getElementById("root"));
 
 // ------------------------------------------------------------------------------
 // destructure props + child props
@@ -129,8 +138,7 @@ const firstBook = {
   author: "Amelia Hepworth",
 };
 const secondBook = {
-  imgLink:
-    "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
+  imgLink: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
   title: "Our Class is a family",
   author: "Shannon Olsen",
 };
@@ -145,10 +153,10 @@ function BoookList() {
       >
         <h3>Description of the book (children 1)</h3>
         <p>
-          (children2) Lorem ipsum dolor sit amet, consectetur adipisicing
-          elit. Saepe, consectetur distinctio? Odio consequuntur iste
-          asperiores tempore atque voluptatum ipsam possimus. Libero iusto
-          doloribus non eligendi ipsum enim eum veritatis ullam!
+          (children2) Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Saepe, consectetur distinctio? Odio consequuntur iste asperiores
+          tempore atque voluptatum ipsam possimus. Libero iusto doloribus non
+          eligendi ipsum enim eum veritatis ullam!
         </p>
       </Book>
       <Book
@@ -156,13 +164,7 @@ function BoookList() {
         imgLink={secondBook.imgLink}
         author={secondBook.author}
       >
-        <p>
-          {" "}
-          Teachers do so much more than just teach academics. They build a
-          sense of community within their classrooms, creating a home away
-          from home where they make their students feel safe, included, and
-          loved.
-        </p>
+        <button>click here</button>
       </Book>
     </section>
   );
