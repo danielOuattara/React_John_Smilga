48bc2a47f333fc48b4616e0a54aa298ad63aae07
danielOuattara <daniel.ouattara@gmx.com>
2023-02-09 21:07:58 +0100

    workin with Vite base

---
+++ 25_advanced-react-v18-vite/src/App.jsx
@@ -0,0 +1,21 @@
+import Test from "./tutorial/01-useState/00-test";
+// import Error from "./tutorial/01-useState/01-error-example";
+// import UseStateBasics from "./tutorial/01-useState/02-useState-basics";
+// import UseStateArray from "./tutorial/01-useState/03-useState-array";
+// import UseStateObject from "./tutorial/01-useState/04-useState-object";
+// import UseStateGotcha from "./tutorial/01-useState/05-useState-gotcha";
+
+function App() {
+  return (
+    <div className="container">
+      <Test />
+      {/* <Error /> */}
+      {/* <UseStateBasics /> */}
+      {/* <UseStateArray /> */}
+      {/* <UseStateObject /> */}
+      {/* <UseStateGotcha /> */}
+    </div>
+  );
+}
+
+export default App;
