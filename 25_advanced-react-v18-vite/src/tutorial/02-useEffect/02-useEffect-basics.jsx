a9df2549c2de8845a6c86a369445eddaf73431f9
danielOuattara <daniel.ouattara@gmx.com>
2023-02-10 16:43:15 +0100

    useEffect

---
+++ 25_advanced-react-v18-vite/src/tutorial/02-useEffect/02-useEffect-basics.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+const UseEffectBasics = () => {
+  const [value, setValue] = useState(0);
+  const sayHello = () => {
+    console.log("hello world");
+  };
+
+  sayHello();
+
+  useEffect(() => console.log("Hello 1 from within useEffect"));
+  useEffect(() => console.log("Hello 2 from within useEffect"), []);
+
+  return (
+    <div>
+      <h1>value : {value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        click me
+      </button>
+    </div>
+  );
+};
+export default UseEffectBasics;
---
+++ 25_advanced-react-v18-vite/src/tutorial/02-useEffect/04-fetch-data.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+
+const url = "https://api.github.com/users";
+
+const FetchData = () => {
+  const [users, setUsers] = useState([]);
+
+  // const fetchUsers = async () => {
+  //   try {
+  //     setUsers(async () => await (await fetch(url)).json());
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  //------------------------------------
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const data = await fetch(url);
+        const users = await data.json();
+        setUsers(() => users);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchUsers();
+  }, []);
+
+  //------------------------------------
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const data = await fetch(url);
+        const users = await data.json();
+        setUsers(() => users);
+        // ??? ---v
+        // setUsers(async () => await (await fetch(url)).json());
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <h2>fetch data example</h2>
+      <ul className="users">
+        {users.map((user) => (
+          <li key={user.id}>
+            <img src={user.avatar_url} alt={user.login} />
+            <div>
+              <h4>{user.login}</h4>
+              <a href={user.html_url} target="_blank" rel="noreferrer">
+                {" "}
+                Link to {user.login}
+              </a>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+export default FetchData;
