57a3a6eabd533636b248f95e0d6b0b87879a29ac
danielOuattara <daniel.ouattara@gmx.com>
2023-03-13 14:04:48 +0100

    update + correct logic for useRef

--- 15_advanced_useRef/src/components/1-useRef-basics.js
+++ 15_advanced_useRef/src/components/1-useRef-basics.js
@@ -5,36 +5,44 @@ import React, { useEffect, useRef } from "react";
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-
   const inputRefContainer = useRef(null);
   const btnRefContainer = useRef(null);
-  const divContainer = useRef(null);
+  const divRefContainer = useRef(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // console.log(inputRefContainer) // Obj { current: input }
-    // console.log(inputRefContainer.current) // <input type="text">
-    // console.log(inputRefContainer.current.value)  // eapiphgr
-    // console.log(btnRefContainer)  // Obj { current: button }
-    // console.log(btnRefContainer.current) // <button type="submit">
-    // console.log(btnRefContainer.current.textContent)  // empty String
-    console.log(divContainer.current)
-  }
+    console.log("--------------------");
+    console.log(inputRefContainer); // Obj { current: input }
+    console.log(inputRefContainer.current); // <input type="text">
+    console.log(inputRefContainer.current.value); // some text
+    //
+    console.log("--------------------");
+
+    console.log(btnRefContainer); // Obj { current: button }
+    console.log(btnRefContainer.current); // <button type="submit">
+    console.log(btnRefContainer.current.textContent); // submit data
+    //
+    console.log("--------------------");
+
+    console.log(divRefContainer.current);
+  };
 
   useEffect(() => {
-    console.log(inputRefContainer.current)
+    console.log(inputRefContainer.current);
     inputRefContainer.current.focus();
-  })
+  });
 
   return (
     <>
       <form action="" className="form" onSubmit={handleSubmit}>
         <div>
           <input type="text" ref={inputRefContainer} />
-          <button type="submit" ref={btnRefContainer} >submit data</button>
+          <button type="submit" ref={btnRefContainer}>
+            submit data
+          </button>
         </div>
       </form>
-      <div ref={divContainer}>hello world</div>
+      <div ref={divRefContainer}>hello world</div>
     </>
   );
 };
