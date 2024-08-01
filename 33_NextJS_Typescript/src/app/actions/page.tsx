a24bda0b0031fcda89e236249e7f226244afd3cb
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-08-01 11:39:02 +0200

    33 Next.js - Typescript: Server Actions: start up

---
+++ 33_NextJS_Typescript/src/app/actions/page.tsx
@@ -0,0 +1,11 @@
+import Form from "@/components/Form";
+import UserList from "@/components/UserList";
+
+export default function ActionsPage() {
+  return (
+    <>
+      <Form />
+      <UserList />
+    </>
+  );
+}
---
+++ 33_NextJS_Typescript/src/components/Form.tsx
@@ -0,0 +1,35 @@
+export default function Form() {
+  return (
+    <form
+      action={createUser}
+      className="max-w-lg flex flex-col gap-y-4 shadow rounded p-8"
+    >
+      <h2 className="text-2xl capitalize mb-4">create user</h2>
+      <input
+        type="text"
+        name="firstName"
+        required
+        className="border shadow rounded py-2 px-3 text-gray-700"
+        defaultValue="peter"
+      />
+      <input
+        type="text"
+        name="lastName"
+        required
+        className="border shadow rounded py-2 px-3 text-gray-700"
+        defaultValue="smith"
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded capitalize"
+      >
+        submit
+      </button>
+    </form>
+  );
+}
+
+const createUser = async () => {
+  "use server";
+  console.log("creating user....");
+};
